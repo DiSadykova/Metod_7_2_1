570abc00738a9563f1d2ad3af85cd1f33e66ed28
Диляра Садыкова <dilyara.mozhet.sadykova@gmail.com>
2021-12-02 22:10:55 +0300

    Решена задача 7_2_1 и 7_2_2

--- Metod_7_2_1/Program.cs
+++ Metod_7_2_1/Program.cs
@@ -10,31 +10,46 @@ namespace Metod_7_2_1
     {
         static void Main(string[] args)
         {
-            double a = MaxSTriangle(3, 4, 5, 4, 5, 6);
-            Console.WriteLine(a);
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            double z1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            double z2 = Convert.ToDouble(Console.ReadLine());
+            double s;
+            byte i;
+            MaxSTriangle(x1, y1, z1, x2, y2, z2, out  s, out  i);
+            Console.WriteLine("Треугольник с большей площадью - № {0}",i);
+            Console.WriteLine("Площадь большего треугольника{0:.00}", s);
             Console.ReadKey();
 
-            double MaxSTriangle(double x1, double y1, double z1, double x2, double y2, double z2)
+
+        }
+        static void MaxSTriangle(double x1, double y1, double z1, double x2, double y2, double z2, out double s, out byte i)
+        {
+            double p1 = (x1 + y1 + z1) / 2;
+            double s1 = Math.Sqrt(p1 * (p1 - x1) * (p1 - y1) * (p1 - z1));
+            double p2 = (x2 + y2 + z2) / 2;
+            double s2 = Math.Sqrt(p2 * (p2 - x1) * (p2 - y1) * (p2 - z1));
+            double d = s1 - s2;
+            s = 0;
+            i = 0;
+            if (d > 0)
             {
-                double p1 = (x1 + y1 + z1) / 2;
-                double s1 = Math.Sqrt(p1 * (p1 - x1) * (p1 - y1) * (p1 - z1));
-                double p2 = (x2 + y2 + z2) / 2;
-                double s2 = Math.Sqrt(p2 * (p2 - x1) * (p2 - y1) * (p2 - z1));
-                double d = s1 - s2;
-                byte i = 0;
-                if (d > 0)
-                {
-                    i = 1;
-                }
-                if (d < 0)
-                {
-                    i = 2;
-                }
-                if (d == 0)
-                {
-                    i = 0;
-                }
-                return i;
+                i = 1;
+                s = s1;
+
+            }
+            if (d < 0)
+            {
+                i = 2;
+                s = s2;
+            }
+            if (d == 0)
+            {
+                i = 0;
+                s = s1;
             }
         }
     }
